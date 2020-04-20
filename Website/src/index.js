2d564e41cff72cdadf96e41e1f0ea044b978007e
John Shortland <John.Shortland@PikiTech.co.nz>
2020-04-21 11:31:02 +1200

    Fixed 'Missing TKoT.scss reference in Website'  issue.

--- Website/src/index.js
+++ Website/src/index.js
@@ -22,6 +22,7 @@ import { BrowserRouter, Route, Switch /*, Redirect */ } from "react-router-dom";
 // styles for this kit
 import "assets/css/bootstrap.min.css";
 import "assets/scss/now-ui-kit.scss";
+import "assets/scss/tkot.scss?v=1.0.0";
 import "assets/demo/demo.css";
 import "assets/demo/nucleo-icons-page-styles.css";
 // pages for this kit
