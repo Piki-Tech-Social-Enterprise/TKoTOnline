b2dd46be066471a19a256a80b5fd3245d388879a
Peke-tupaea <pekemanawaiti@gmail.com>
2020-04-29 13:52:07 +1200

    intial commit

---
+++ PWA/src/views/Auth/AuthCommunityLinkView.jsx
@@ -0,0 +1,15 @@
+import React, {
+    useState,
+    useEffect
+  } from 'react';
+  
+  const AuthCommunityLinkView = props => {
+
+    return (
+      <>
+      <h1>Community child link page</h1>
+      </>
+    )
+  };
+  
+  export default AuthCommunityLinkView;
---
+++ PWA/src/views/Auth/AuthCommunityLinksView.jsx
@@ -0,0 +1,16 @@
+import React, {
+    useState,
+    useEffect
+  } from 'react';
+  
+  const AuthCommunityLinksView = props => {
+
+    return (
+      <>
+      <h1>Community links</h1>
+      </>
+    )
+  };
+  
+  export default AuthCommunityLinksView;
+  
