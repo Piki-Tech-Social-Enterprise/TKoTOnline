0589c1cb79a741854455738687814c8ebb678937
John Shortland <John.Shortland@PikiTech.co.nz>
2020-05-14 15:18:28 +1200

    Refactored About page in Website.

--- Website/src/views/AboutUsView.jsx
+++ Website/src/views/AboutUsView.jsx
@@ -1,4 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React, {
+  useEffect,
+  useState
+} from 'react';
 import {
   Container,
   Row,
@@ -9,23 +12,29 @@ import HomeFooter from 'components/Footers/HomeFooter';
 import {
   withFirebase
 } from 'components/Firebase';
+import LoadingSpinner from 'components/App/LoadingSpinner';
 
 const AboutUsView = props => {
-
-  const [aboutPageDescription, setAboutState] = useState ([]);
-
-useEffect(() => {
-
-  const retrieveSettingValues = async () => {
-    const {
-      firebase
-    } = props;
-    const dbSettings = await firebase.getDbSettingsValues(true);
-    setAboutState(((dbSettings && dbSettings.aboutPageDescription) || ''));
-  }
-
-  retrieveSettingValues();
-}, [props])
+  const [state, setState] = useState({
+    isLoading: true,
+    aboutPageDescription: ''
+  });
+  useEffect(() => {
+    const retrieveSettingValues = async () => {
+      const {
+        firebase
+      } = props;
+      const dbSettings = await firebase.getDbSettingsValues(true);
+      setState(s => ({
+        ...s,
+        isLoading: false,
+        aboutPageDescription: ((dbSettings && dbSettings.aboutPageDescription) || s.aboutPageDescription || '')
+      }));
+    }
+    if (state.isLoading) {
+      retrieveSettingValues();
+    }
+  }, [props, state])
   return (
     <>
       <HomeNavbar />
@@ -33,7 +42,11 @@ useEffect(() => {
         <Row>
           <Col className="px-0 mt-5">
             <h3>ABOUT US</h3>
-            <p>{aboutPageDescription}</p>
+            {
+              state.isLoading
+                ? <LoadingSpinner outerClassName="ignore" innerClassName="ignore" />
+                : <p>{state.aboutPageDescription}</p>
+            }
           </Col>
         </Row>
       </Container>
