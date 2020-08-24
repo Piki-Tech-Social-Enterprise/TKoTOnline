29457cd3a2fa564447feae7bb42f2a35b46a17bb
John Shortland <John.Shortland@PikiTech.co.nz>
2020-08-24 16:18:44 +1200

    Continued to implement more Lighthouse suggestions for better web performance in Website.

--- Website/src/components/Footers/HomeFooter.jsx
+++ Website/src/components/Footers/HomeFooter.jsx
@@ -268,7 +268,7 @@ const HomeFooter = props => {
           <Col xs={12} lg={6} className="my-auto text-lg-right bg-success1">
             <span className="text-uppercase font-weight-bolder my-0 mt-3 mt-sm-0 mt-lg-3 mr-3 mr-sm-0 mr-lg-5">Website co-curated by</span> <br className="d-md-none" />
             {/* <a href="#TKoTOnline" title="Making Everything Achievable Limited" target="_blank" rel="noopener noreferrer"> */}
-            <img className="ml-0 ml-sm-3 created-by-logo-image" alt="Making Everything Achievable Limited" src={require('assets/img/tkot/mea-logo-165x165.png')} />
+            <img className="ml-0 ml-sm-3 created-by-logo-image" alt="Making Everything Achievable Limited" src={require('assets/img/tkot/mea-logo-165x165.webp')} />
             {/* </a> */}
             <a href="https://PikiTech.co.nz" title="Piki Tech Limited" target="_blank" rel="noopener noreferrer" onClick={() => sendEvent(`${window.location.pathname} page`, 'Clicked "PikiTech.co.nz" link')}>
               <img className="ml-3 created-by-logo-image" alt="Piki Tech Limited" src={require('assets/img/tkot/piki-tech-logo-white-transparent-165x165.webp')} />
