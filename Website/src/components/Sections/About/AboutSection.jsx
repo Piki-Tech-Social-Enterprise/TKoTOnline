5dc5368fcaf5a2c80fa69850ea1a9d574a3e9fc1
John Shortland <John.Shortland@PikiTech.co.nz>
2020-09-28 11:52:08 +1300

    Revert "Implemented fullscreen TKoT Video on Home page in Website."
    This reverts commit 188696db8677dd7ac8452984d0d1d47066cca510.

--- Website/src/components/Sections/About/AboutSection.jsx
+++ Website/src/components/Sections/About/AboutSection.jsx
@@ -1,15 +1,32 @@
 import React, {
   useState,
-  useEffect
+  useEffect,
+  lazy
 } from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Button
+} from 'reactstrap';
 import {
   withFirebase
 } from 'components/Firebase';
+import Routes from 'components/Routes/routes';
+import {
+  sendEvent
+} from 'components/App/GoogleAnalytics';
 import tkotVideo from '../../../assets/videos/v2.mp4';
 
+const LoadingSpinner = lazy(async () => await import('components/App/LoadingSpinner'));
+const {
+  aboutUs,
+  projects
+} = Routes;
 const AboutSection = props => {
   const {
-    pageAboutImage
+    pageAboutImage,
+    showClickScrollDownForMoreLink
   } = props;
   const [state, setState] = useState({
     isLoading: true,
@@ -40,14 +57,48 @@ const AboutSection = props => {
       <div className="about-image" style={{
         backgroundImage: `linear-gradient(183deg, rgba(0, 0, 0, 0.83), rgba(0, 0, 0, 0)), url(${pageAboutImage})`
       }}>
-        <div className="videoHeader">
-          <video playsInline autoPlay muted loop>
-            <source
-              type="video/mp4"
-              src={tkotVideo}
-            />
-          </video>
-        </div>
+        <Container className="py-5 text-center">
+          <Row>
+            <Col xs={12} sm={6} className="bg-danger1">
+              {/* <img alt="..." className="n-logo pt-2 my-1 lazyload" data-src={require("assets/img/tkot/tkot-white-logo.webp")} style={{
+                width: 419
+              }} /> */}
+              <div className="videoHeader">
+                <video playsInline autoPlay muted loop>
+                  <source
+                    type="video/mp4"
+                    src={tkotVideo}
+                  />
+                </video>
+              </div>
+            </Col>
+            <Col xs={12} sm={6} className="text-left text-white h5 pt-5 pt-sm-0 my-auto bg-warning1">
+              <div className="my-3">
+                {
+                  state.isLoading
+                    ? <LoadingSpinner outerClassName="ignore" innerClassName="ignore" />
+                    : <>
+                      <span>{state.settings.homePageAboutDescription}</span><br />
+                      <Button
+                        href={aboutUs}
+                        className="tkot-primary-red-bg-color-50-pc btn-outline-light my-3 mt-lg-5 mb-lg-1"
+                        color="white"
+                        onClick={() => sendEvent('Home page', 'Clicked "Learn More..." button')}
+                      >Learn more...
+                      </Button>
+                    </>
+                }
+              </div>
+            </Col>
+          </Row>
+          {
+            showClickScrollDownForMoreLink
+              ? <a href={projects} className="text-decoration-none text-dark">
+                <p className="my-0 mt-5"><i className="fas fa-angle-double-down" /> Click/Scroll down for more <i className="fas fa-angle-double-down" /></p>
+              </a>
+              : null
+          }
+        </Container>
       </div>
     </div>
   );
